d6c492b440df9d45f7b87c7d880011cea5ff7b35
K. R <rawsonkara@gmail.com>
2024-01-23 21:53:40 -0500

    Copied latest model over into TFModels for sharing

--- RagdollTrainer/Assets/Scripts/TargetController.cs
+++ RagdollTrainer/Assets/Scripts/TargetController.cs
@@ -3,74 +3,117 @@ using Random = UnityEngine.Random;
 
 namespace Unity.MLAgentsExamples
 {
-    /// <summary>
-    /// Utility class to allow target placement and collision detection with an agent
-    /// Add this script to the target you want the agent to touch.
-    /// Callbacks will be triggered any time the target is touched with a collider tagged as 'tagToDetect'
-    /// </summary>
     public class TargetController : MonoBehaviour
     {
-        [SerializeField] LayerMask layerMask;
-        [SerializeField] float rayDown = 10;
-
-        public Vector2 spawnX = new Vector2(-10, 10); //The region in which a target can be spawned.
-        public Vector2 spawnY = new Vector2(0.5f, 1.5f);
-        public Vector2 spawnZ = new Vector2(-10, 10);
-
-        public bool respawnIfTouched = true; //Should the target respawn to a different position when touched
-
-        public bool m_LocalRespawn = false;
-
         const string k_Agent = "agent";
+
+        [SerializeField] LayerMask m_LayerMask;
+        [SerializeField] float m_RayDown = 10;
+
+        [Header("Target Spawning RNG")]
+        [Tooltip("The range of the x-coordinate for the target sphere's spawn position.")]
+        public Vector2 m_SpawnX = new Vector2(-9, 9);
+        [Tooltip("The range of the y-coordinate for the target sphere's spawn position. The actual y-coordinate will be adjusted based on the raycast hit point.")]
+        public Vector2 m_SpawnY = new Vector2(0.5f, 1.5f);
+        [Tooltip("The range of the z-coordinate for the target sphere's spawn position.")]
+        public Vector2 m_SpawnZ = new Vector2(-9, 9);
+
+        [Header("Target Respawn Options")]
+        public bool m_RespawnIfTouched = true;
+        public bool m_LocalRespawn = false;
+        public bool m_SpawnOnlyOnGround = true;
+
+        [Header("Target Respawn Timer")]
+        [Tooltip("This is the default timer used to make the target respawn. Randomize Respawn Time will override this value. Be careful when setting this value to low or high.")]
+        [Range(1f, 60f)][SerializeField] float m_RespawnTime = 30f;
+        [Tooltip("This is the minimum time which the random respawn time will be.")]
+        [Range(1f, 60f)][SerializeField] float m_MinRespawnTime = 1f;
+        [Tooltip("This is the maximum time which the random respawn time will be.")]
+        [Range(1f, 60f)][SerializeField] float m_MaxRespawnTime = 20f;
+
+        [Tooltip("This will use the min and max respawn times to generate a new time between that.")]
+        public bool m_RandomizeRespawnTime = true;
+
+        float _Timer;
+
+        private void Start()
+        {
+            _Timer = m_RandomizeRespawnTime ? Random.Range(m_MinRespawnTime, m_MaxRespawnTime) : m_RespawnTime;
+        }
+
+        private void Update()
+        {
+            _Timer -= Time.deltaTime;
+
+            if (Mathf.Approximately(_Timer, 0f) || _Timer < 0f)
+            {
+                ResetTarget();
+            }
+        }
 
         void FixedUpdate()
         {
             if (transform.localPosition.y < -5)
             {
-                Debug.Log($"{transform.name} Fell Off Platform");
+                Debug.Log($"{transform.name} Off Platform");
                 MoveTargetToRandomPosition();
             }
         }
 
-        /// <summary>
-        /// Moves target to a random position within specified radius.
-        /// </summary>
+        private void ResetTarget()
+        {
+            MoveTargetToRandomPosition();
+            _Timer = m_RandomizeRespawnTime ? Random.Range(m_RespawnTime / 10, m_RespawnTime) : m_RespawnTime;
+        }
+
         public void MoveTargetToRandomPosition()
         {
             Vector3 newTargetPos;
             Collider[] hitColliders;
+            bool isOnGround = false;
 
             do
             {
                 if (m_LocalRespawn)
                 {
-                    newTargetPos = new Vector3(Random.Range(transform.position.x + spawnX.x, transform.position.x + spawnX.y), rayDown, Random.Range(transform.position.z + spawnZ.x, transform.position.z + spawnZ.y));
+                    newTargetPos = new Vector3(Random.Range(transform.position.x + m_SpawnX.x, transform.position.x + m_SpawnX.y), m_RayDown, Random.Range(transform.position.z + m_SpawnZ.x, transform.position.z + m_SpawnZ.y));
                 }
                 else
                 {
-                    newTargetPos = new Vector3(Random.Range(spawnX.x, spawnX.y), rayDown, Random.Range(spawnZ.x, spawnZ.y));
+                    newTargetPos = new Vector3(Random.Range(m_SpawnX.x, m_SpawnX.y), m_RayDown, Random.Range(m_SpawnZ.x, m_SpawnZ.y));
                 }
 
                 RaycastHit hit;
-                if (Physics.Raycast(newTargetPos, Vector3.down, out hit, Mathf.Infinity, layerMask))
+                if (Physics.Raycast(newTargetPos, Vector3.down, out hit, Mathf.Infinity, m_LayerMask))
                 {
-                    newTargetPos.y = hit.point.y + Random.Range(spawnY.x, spawnY.y);
+                    newTargetPos.y = hit.point.y + Random.Range(m_SpawnY.x, m_SpawnY.y);
+                }
+
+                if (m_SpawnOnlyOnGround)
+                {
+                    isOnGround = Physics.Raycast(newTargetPos, Vector3.down, out RaycastHit _, Mathf.Infinity, m_LayerMask) ||
+                                 Physics.CheckSphere(newTargetPos, transform.localScale.x / 2, m_LayerMask);
                 }
 
                 hitColliders = Physics.OverlapSphere(newTargetPos, transform.localScale.x / 2);
 
+                if (!isOnGround)
+                {
+                    continue;
+                }
+
             } while (hitColliders.Length > 0);
 
-            transform.localPosition = newTargetPos; //Use local position
+            transform.localPosition = newTargetPos;
         }
 
         private void OnCollisionEnter(Collision col)
         {
             if (col.transform.CompareTag(k_Agent))
             {
-                if (respawnIfTouched)
+                if (m_RespawnIfTouched)
                 {
-                    MoveTargetToRandomPosition();
+                    ResetTarget();
                 }
             }
         }
