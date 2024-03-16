a82408624edfbff895a5885446c69196aed0bf21
K. R. <rawsonkara@gmail.com>
2024-03-16 16:05:28 -0400

    make sure we are properly exporting the correct onnx models into our TFModels asset directory

--- bin/copy_onnx_files.bat
+++ bin/copy_onnx_files.bat
@@ -2,23 +2,17 @@
 set SOURCE_PATH=C:\Users\3nigma\Unity\NekoCatGame\RagdollTrainer\results\KyleBeta2.b0a-020m\Kyle\
 set DESTINATION_PATH=C:\Users\3nigma\source\repos\Kyle-b0a\models\
 
-echo Copying ONNX files from "%SOURCE_PATH%" to "%DESTINATION_PATH%"...
-xcopy "%SOURCE_PATH%*.onnx" "%DESTINATION_PATH%" /Y
-
-echo Renaming copied ONNX files...
-cd /d "%DESTINATION_PATH%"
-for %%f in (Kyle-*.onnx) do (
+cd /d "%SOURCE_PATH%"
+for %%f in (*.onnx) do (
+    echo Copying and renaming: "%%f"
+    xcopy "%%f" "%DESTINATION_PATH%" /Y > NUL
     set "filename=%%~nf"
     setlocal enabledelayedexpansion
     if not "!filename:~0,9!"=="Kyle-b0a-" (
         set "newname=Kyle-b0a-!filename:~5!"
-        echo Renaming: "%%f" to "!newname!.onnx"
-        ren "%%f" "!newname!.onnx"
+        move /Y "%DESTINATION_PATH%%%f" "%DESTINATION_PATH%!newname!.onnx" > NUL
     )
     endlocal
 )
 
 echo Operation completed.
-
-:exit
-exit /b 0
